6cbaa2db713eb2acbe861ea854b848862116e8a7
Mrittika Dutta <129971118+MrittikaDutta@users.noreply.github.com>
2025-07-13 10:51:33 +0530

    Create 2410_PlayersMatch.py

---
+++ 2410_PlayersMatch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+        
+        i = j = matches = 0
+        
+        while i < len(players) and j < len(trainers):
+            if players[i] <= trainers[j]:
+                matches += 1
+                i += 1
+                j += 1
+            else:
+                j += 1  # Trainer too weak, try next trainer
+        
+        return matches
+
