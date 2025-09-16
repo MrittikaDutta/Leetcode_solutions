2225ebe4888f9b18d7cbac9127cc81928d2a0d06
Mrittika Dutta <129971118+MrittikaDutta@users.noreply.github.com>
2025-09-16 10:17:47 +0530

    Create 1935. Maximum Number of Words You Can Type.py

---
+++ September 2025/1935. Maximum Number of Words You Can Type.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        broken = set(brokenLetters)
+        count = 0
+
+        for word in text.split():
+            if not any(ch in broken for ch in word):
+                count += 1
+
+        return count
